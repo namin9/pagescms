124bc15ff5fa83d5bf6cec082ab8990a7c629f6d
namin9 <koolee1372@gmail.com>
2026-04-13 15:18:42 +0900

    OpenNext로 마이그레이션

--- app/(auth)/sign-in/collaborator/page.tsx
+++ app/(auth)/sign-in/collaborator/page.tsx
@@ -18,7 +18,7 @@ import {
   EmptyTitle,
 } from "@/components/ui/empty";
 
-export const runtime = "edge";
+
 
 const isInviteEmailMatch = (signedInEmail?: string, inviteEmail?: string) => {
   if (!signedInEmail || !inviteEmail) return false;
