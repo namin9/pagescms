7972621b3fc05b2316c7214591f0146c7a736c4c
namin9 <koolee1372@gmail.com>
2026-04-13 14:53:58 +0900

    of

--- db/envConfig.ts
+++ db/envConfig.ts
@@ -1,6 +1,7 @@
 import { loadEnvConfig } from "@next/env";
- 
-if (process.env.NEXT_RUNTIME !== "edge" && process.env.NODE_ENV !== "production") {
-  const projectDir = process.cwd();
-  loadEnvConfig(projectDir);
+
+if (process.env.NEXT_RUNTIME !== "edge" && process.env.NODE_ENV === "development") {
+  // Only call process.cwd() when we are sure we are in a Node.js development environment.
+  const getProjectDir = () => process.cwd();
+  loadEnvConfig(getProjectDir());
 }
